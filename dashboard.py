b25cfbcfbc258646ae47eaf38c3b67cf899d435c
Tyson Cheah <yichaocheah99@gmail.com>
2026-04-20 12:21:49 +0800

    config secret

--- dashboard.py
+++ dashboard.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
+from google.oauth2 import service_account
 from google.cloud import bigquery
 
 credentials = service_account.Credentials.from_service_account_info(
